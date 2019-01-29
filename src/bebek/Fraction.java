a382621782177af81271d01ca380586845637580
dvckbass <duckbass@live.com>
2019-01-29 10:07:01 +0100

    part 1

---
+++ src/bebek/Fraction.java
@@ -0,0 +1,109 @@
+package bebek;
+
+public class Fraction {
+	private int num;
+	private int den;
+
+	public Fraction(int num, int den) {
+		if (den == 0) {
+			throw new IllegalArgumentException("den can't be 0");
+		} else if (den < 0) {
+			this.num = num * (-1);
+			this.den = den * (-1);
+		} else {
+			this.num = num;
+			this.den = den;
+		}
+	}
+
+	public Fraction(int num) {
+		this.num = num;
+		this.den = 1;
+	}
+
+	public Fraction() {
+		this.num = 0;
+		this.den = 1;
+	}
+
+	public int getnum() {
+		return num;
+	}
+
+	public int getden() {
+		return den;
+	}
+
+	@Override
+	public String toString() {
+		return num + "/" + den;
+	}
+
+	public double toDouble() {
+		return num / den;
+	}
+
+	public Fraction add(Fraction other) {
+		//result of new numerator
+		int numRes = (this.num * other.den) + (other.num * other.den);
+		//result of new denominator
+		int denRes = this.den * other.den;
+		return new Fraction(numRes, denRes);
+	}
+
+	public Fraction substract(Fraction other) {
+		//result of new numerator
+		int numRes = (this.num * other.den) - (other.num * other.den);
+		//result of new denominator
+		int denRes = this.den * other.den;
+		return new Fraction(numRes, denRes);
+	}
+
+	public Fraction multiply(Fraction other) {
+		//result of new numerator
+		int numRes = this.num * other.num;
+		//result of new denominator
+		int denRes = this.den * other.den;
+		return new Fraction(numRes, denRes);
+	}
+
+	public Fraction divide(Fraction other) {
+		//result of new numerator
+		int numRes = this.num * other.den;
+		//result of new denominator
+		int denRes = this.den * other.num;
+		return new Fraction(numRes, denRes);
+	}
+
+	public boolean equals(Object other) {
+		if(other instanceof Fraction) {
+			Fraction frac = (Fraction) other;
+			if(this.num > frac.num) {
+				if(this.num % frac.num == 0 && this.den % frac.den == 0) {
+					return true;
+				}
+			} else if(frac.num % this.num == 0 & frac.den % this.den == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void toLowestTerms() {
+		int gcd = gcd(this.num, this.den);
+		this.num /= gcd;
+		this.den /= gcd;
+	}
+
+	//find greatest common divisor
+	public static int gcd(int num, int den) {
+		int tmp = 0;
+		while(num != 0 && den != 0) {
+			tmp = num % den;
+			num = den;
+			den = tmp;
+		}
+		return tmp;
+	}
+
+}
